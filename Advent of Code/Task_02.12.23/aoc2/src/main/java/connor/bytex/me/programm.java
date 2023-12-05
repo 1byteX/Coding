e821746f36edeb1672c64270fcd9856f2ea1b83c
byteX <mlohr55@outlook.de>
2023-12-05 17:42:23 +0100

    Kleine Korrekturen im Package

--- Advent of Code/Task_02.12.23/aoc2/src/main/java/connor/bytex/me/programm.java
+++ Advent of Code/Task_02.12.23/aoc2/src/main/java/connor/bytex/me/programm.java
@@ -1,10 +1,17 @@
+package connor.bytex.me;
+
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class programm {
     public static Scanner scanner;
     public static void main(String[] args) {
-        scanner = new Scanner(new File("input.csv"), "UTF-8");
+        try {
+            scanner = new Scanner(new File("input.csv"), "UTF-8");
+        } catch (FileNotFoundException e) {
+            System.out.println("Datei nicht gefunden!");
+        }
         System.out.println(scanner.next());
     }
 }
